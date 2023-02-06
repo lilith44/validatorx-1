a6813909d6e59d8e31590fe0a57663ff872435f7
陈遇文 <chenyuwen@class100.com>
2023-02-06 10:28:01 +0800

    fix cn_en_num_end_begin_no_space validate

--- name_test.go
+++ name_test.go
@@ -23,13 +23,13 @@ func Test_ChineseOrEnglishOrNumbersOrSpace(t *testing.T) {
 
 	t.Run(`valid_han_num`, func(t *testing.T) {
 		if !ChineseOrEnglishOrNumbersOrSpace(`汉字123`) {
-			t.Errorf(`{测试 空格数字123asasd}测试未通过`)
+			t.Errorf(`{汉字123}测试未通过`)
 		}
 	})
 
-	t.Run(`valid_han_num`, func(t *testing.T) {
-		if !ChineseOrEnglishOrNumbersOrSpace(`汉字123`) {
-			t.Errorf(`{汉字123}测试未通过`)
+	t.Run(`valid_han_space_num`, func(t *testing.T) {
+		if !ChineseOrEnglishOrNumbersOrSpace(`汉字  123`) {
+			t.Errorf(`{汉字  123}测试未通过`)
 		}
 	})
 
