71f7a4756af9084df26bb03ba43e025fc12d3ceb
陈遇文 <chenyuwen@class100.com>
2023-02-06 10:26:46 +0800

    fix cn_en_num_end_begin_no_space validate

--- name_test.go
+++ name_test.go
@@ -3,27 +3,39 @@ package validatorx
 import "testing"
 
 func Test_ChineseOrEnglishOrNumbersOrSpace(t *testing.T) {
-	t.Run(`invalid_zh_space_num`, func(t *testing.T) {
+	t.Run(`valid_zh_space_num`, func(t *testing.T) {
 		if !ChineseOrEnglishOrNumbersOrSpace(`测试 空格数字123`) {
 			t.Errorf(`{测试 空格数字123}测试未通过`)
 		}
 	})
 
-	t.Run(`invalid_han_space_num_en`, func(t *testing.T) {
+	t.Run(`valid_han_space_num_en`, func(t *testing.T) {
 		if !ChineseOrEnglishOrNumbersOrSpace(`测试 空格数字123asasd`) {
 			t.Errorf(`{测试 空格数字123asasd}测试未通过`)
 		}
 	})
 
-	t.Run(`invalid_han_num_en`, func(t *testing.T) {
+	t.Run(`valid_han_num_en`, func(t *testing.T) {
 		if !ChineseOrEnglishOrNumbersOrSpace(`撒大苏打123123efef`) {
 			t.Errorf(`{测试 空格数字123asasd}测试未通过`)
 		}
 	})
 
-	t.Run(`invalid_han_num`, func(t *testing.T) {
+	t.Run(`valid_han_num`, func(t *testing.T) {
 		if !ChineseOrEnglishOrNumbersOrSpace(`汉字123`) {
 			t.Errorf(`{测试 空格数字123asasd}测试未通过`)
+		}
+	})
+
+	t.Run(`valid_han_num`, func(t *testing.T) {
+		if !ChineseOrEnglishOrNumbersOrSpace(`汉字123`) {
+			t.Errorf(`{汉字123}测试未通过`)
+		}
+	})
+
+	t.Run(`invalid_han_num`, func(t *testing.T) {
+		if ChineseOrEnglishOrNumbersOrSpace(`汉字123！@#`) {
+			t.Errorf(`{汉字123！@#}测试未通过`)
 		}
 	})
 }
